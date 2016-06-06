b466b235c57b888ca8f50b66d0856585fdb77dea
takashima <k014c1125@it-neec.jp>
2016-06-06 14:05:45 +0900

    add

--- db.sh
+++ db.sh
@@ -8,6 +8,6 @@ mysql -uroot -proot -e "create database board character set utf8;"
 
 #list作成
 mysql -uroot -proot -e "create table list(id MEDIUMINT NOT NULL AUTO_INCREMENT
-, item varchar(256), PRIMARY KEY (id))"
-
-mysql -uroot -proot -e "select current_timestamp board;"
+, item varchar(256), PRIMARY KEY (id), ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  dt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP;
+)"
