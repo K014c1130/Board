7060677d15b73df30a4db1b0fb5b998bf18ddc07
三浦 <k014c1138@it-neec.jp>
2016-06-06 12:45:12 +0900

    add a line

--- board.php
+++ board.php
@@ -1,1 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title> 掲示板</title>
+</head>
+  <?php
+  $name = $_POST['name'];
+  $time = $_POST['name'];
 
+  $sql = "SELECT name,time FROM DB
+          ORDER BY time DESC"
+  $stmt = $dbh->prepare($sql);
+  $stmt -> execute();
+
+  while(true){
+    $rec = $stmt->fetch(PDO::FETCH_ASSOC);
+    if($rec==false){
+      $dbh = null;
+      break;
+    }
+    print $rec['name'];
+    print "  ";
+    print $rec['time'];
+  ?>
+    <input type = "submmit" name="delete" value="delete">
+    <?php print "<br />" ?>
+  }
+
+</body>
+    </html>
