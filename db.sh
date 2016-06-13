c7434e1beef52655bbda6c2cb4771b9bddba0fb2
takashima <k014c1125@it-neec.jp>
2016-06-13 13:51:28 +0900

    add a line

--- db.sh
+++ db.sh
@@ -9,5 +9,5 @@ mysql -uroot  -e "create database board character set utf8;"
 
 #list作成
 mysql -uroot board -e "create table list(id MEDIUMINT NOT NULL AUTO_INCREMENT
-, name varchar(256),massage varchar(256), ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+, name varchar(256),massage varchar(256),pass varchar(256), ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   dt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, PRIMARY KEY (id));"
