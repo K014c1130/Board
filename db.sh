35dae55db05e3491aed95951e7a02e0779aa8a44
takashima <k014c1125@it-neec.jp>
2016-06-13 14:08:55 +0900

    add

--- db.sh
+++ db.sh
@@ -7,11 +7,10 @@ mysql -uroot  -e "drop database board;"
 mysql -uroot  -e "create database board character set utf8;"
 
 
-#list作成
+
+
+
+#list作成j
 mysql -uroot board -e "create table list(id MEDIUMINT NOT NULL AUTO_INCREMENT
-<<<<<<< HEAD
-, name varchar(256),massage varchar(256),pass varchar(256), ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-=======
-, name varchar(256) NOT NULL,massage varchar(256), ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
->>>>>>> b47e046e0229ad04c93f046ee5abc572080882f6
+, name varchar(256) NOT NULL,massage varchar(256),pass varchar(256), ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   dt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, PRIMARY KEY (id));"
