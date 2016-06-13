f24b4f731acd6f964d8123db235c896d3c06a664
K014c1130 <k014c1130@it-neec.jp>
2016-06-13 09:52:37 +0900

    aal

--- board.php
+++ board.php
@@ -1,1 +1,20 @@
+<DOCTYPE html>
+  <html lang="ja">
+  <head>
+  <meta charset="UTF-8">
+  <title>login</title>
+</head>
+<body>
+  <h1>掲示板(工事中)</h1>
+こんにちは、
+  <?php
+print htmlspecialchars($_GET['name'],ENT_QUOTES, 'UTF-8');
+  ?>
+さん！
+この掲示板はまだできてないです。
+  <form action="login.php" method="get">
+<input type="submit"  value="戻る">
+</form>
 
+</body>
+</html>
